4ec89eac5aae390feb9f2bb9cb6b1d3591527549
Thibault LUX <tlux@e1r12p14.42.fr>
2018-04-04 14:39:23 +0200

    First push

---
+++ index.php
@@ -0,0 +1,162 @@
+<?php
+
+?>
+<html>
+	<head>
+		<title>Thibault LUX</title>
+		<link rel="stylesheet" href="tlux.css">
+		<link rel="stylesheet" type="text/css" href="resources/fonts/fonts.css" />
+	</head>
+	<body>
+		<div class="fixed">
+			<a href="#home">Home</a>
+		</div>
+		<div class="header" id="home">
+			<IMG src="resources/42copy.jpeg">
+		</div>
+		<div class="avatar">
+			<img src="resources/avatar.png">
+		</div>
+		<div class="name">
+			<h1>Thibault LUX</h1>
+		</div>
+		<div class="job">
+			<h1> <br />Developper.</h1>
+		</div>
+		<div class="index">
+			<div> <a href="#about_me">About me</a></div>
+			<div> <a href="#career">Career</a></div>
+			<div> <a href="#skills">Skills</a></div>
+			<div> <a href="#objectives">Objectives</a></div>
+			<div> <a href="#interests">Interests</a></div>
+			<div> <a href="#contact">Contact</a></div>
+		</div>
+		<div id="about_me">
+			<h1>Nice to meet you !</h1>
+			<p>I'm Thibault, a 19 years old developper.<br />
+			Some words to describe me : logical, cartesian, creative.<br />
+			So, I've always had affinity with code.<br /><br />
+			I work for me. My first motivation is to learn more and more.<br />
+			I'm student at school 42 in Paris since 6 months, <br />
+			It was an opportunity for me to dive into coding's world.<br /><br />
+			But a school environment is still too limited.<br />
+			And now it's time for me to earn professional experience.
+			</p>
+		</div>
+		<div id="career">
+			<div class="highschool">
+				<h1>High school</h1>
+				<img src="resources/highschool.png">
+				<h2>2013 - 2017</h2>
+				<a href="http://www4.ac-nancy-metz.fr/lyc-loritz/index.php"><p>Lycée Henri Loritz</a>, Nancy, France<br /><br />
+				Formation :<br />Scientific baccalaureate (Got)</p>
+			</div>
+			<div class="borntocode">
+				<h1>Studies</h1><br /><br /><br />
+				<img src="resources/42logo.png"><br /><br />
+				<h2>2017 - Now</h2>
+				<a href="http://www.42.fr"><p>42 Born2Code</a>, Paris, France<br /><br />
+				Formation :<br />Architect in Digital Technology</p>
+
+			</div>
+			<div class="jobs">
+	<h1>Professional Experience</h1><br /><br /><br />
+				<img src="resources/job.jpg"><br />
+				<h2>None</h2>
+				<p>Be my first one if you want so !</p>
+
+			</div>
+		</div>
+		<div id="skills">
+			<div class="languages">
+				<h1>Languages</h1>
+				<p>French</p> ★★★★★ 
+				<hr>
+				<p>English</p> ★★★★
+			</div>
+			<div class="expertises">
+				<h1>Expertises</h1>
+				<p>C</p> ★★★★★
+				<hr>
+				<p>Shell and Shellscript</p> ★★★★★
+				<hr>
+				<p>Php</p> ★★★
+				<hr>
+				<p>Css</p> ★★★
+				<hr>
+				<p>Html</p> ★★
+			</div>
+			<div class="environments">
+				<h1>Environments</h1>
+				<p>Windows</p> ★★★★★
+				<hr>
+				<p>MacOS</p> ★★★★★
+				<hr>
+				<p>Linux</p> ★★★
+				<hr>
+				<p>Ubuntu</p> ★★
+			</div>
+			<div class="software">
+				<h1>Software</h1>
+				<p>TeamViewer</p> ★★★★★
+				<hr>
+				<p>Pack Office</p> ★★★★★
+				<hr>
+				<p>After Effects</p> ★★★
+				<hr>
+				<p>Photoshop</p> ★★
+			</div>
+		</div>
+		<div id="objectives">
+			<h1>My objectives :</h1>
+			<div class="for_me">
+			<h1>For me :</h1>
+			<p>The first thing I aim at is professional experience.<br />
+			I never really worked and it's time to prove my worth in company.<br />
+			Secondly, it's a way for me to learn plenty of new things.<br />
+			In internship I hope to discover new languages, softwares, and new ways to think.</p>
+			</div>
+			<div class="for_you">
+			<h1>For you :</h1>
+			<p>As employee or trainee in your company, I will handle every mission carefully.<br />
+			My first objective is to take part of team projects.<br />
+			Our formation at school 42 used us to work as groups.<br />
+			I can lead projects well, bring ideas and respect specifications.<br /></p>
+			</div>
+		</div>
+		<div id="interests">
+		</div>
+		<div id="contact">
+			<div class="pro">
+				<h1>Professional</h1>
+				<div>
+				<a href="mailto:tlux@student.42.fr">
+				<img src="resources/mail.png"><p>tlux@student.42.fr</p></a>
+				</div>
+				<div>
+				<a href="https://www.linkedin.com/in/thibault-lux-11386215a/">
+				<img src="resources/linkedin.png"><p>LinkedIn</p></a>
+				</div>
+				<div>
+				<a href="tel:+33781371888">
+				<img src="resources/phone.png"><p>+33 7 81 37 18 88</p></a>
+				</div>
+			</div>
+			<div class="perso">
+				<h1>Personnal</h1>
+				<div>
+				<a href="mailto:lux.thibault@hotmail.fr">
+				<img src="resources/mail.png"><p>lux.thibault@hotmail.fr</p></a>
+				</div>
+				<div>
+				<a href="facebook.com/luxthibault">
+				<img src="resources/fb.png"><p>Facebook</p></a>
+				</div>
+				<div>
+				<a href="tel:+33781371888">
+				<img src="resources/phone.png"><p>+33 7 81 37 18 88</p></a>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
