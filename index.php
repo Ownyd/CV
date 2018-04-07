b9071ac3eb540d62262e54cb1637dc8b1d3f3c93
Thibault LUX <tlux@e1r11p15.42.fr>
2018-04-07 06:26:33 +0200

    add interest/header

--- index.php
+++ index.php
@@ -9,9 +9,11 @@
 	</head>
 	<body>
 		<div class="fixed">
-			<a href="#home">Home</a>
+			<a href="#about_me"><img src="resources/arrow_up.png"></a>
 		</div>
-		<div class="header" id="home">
+		<div class="margintopper"></div>
+		<div class="hidetopper"></div>
+		<div class="topper">
 			<IMG src="resources/42copy.jpeg">
 		</div>
 		<div class="avatar">
@@ -21,20 +23,21 @@
 			<h1>Thibault LUX</h1>
 		</div>
 		<div class="job">
-			<h1> <br />Developper.</h1>
+			<h3>DEVELOPPER.</h3>
 		</div>
 		<div class="index">
-			<div> <a href="#about_me">About me</a></div>
-			<div> <a href="#career">Career</a></div>
-			<div> <a href="#skills">Skills</a></div>
-			<div> <a href="#objectives">Objectives</a></div>
-			<div> <a href="#interests">Interests</a></div>
-			<div> <a href="#contact">Contact</a></div>
+			<a href="#about_me"><div>About me</div></a>
+			<a href="#career"><div>Career</div></a>
+			<a href="#skills"><div>Skills</div></a>
+			<a href="#objectives"><div>Objectives</div></a>
+			<a href="#interests"><div>Interests</div></a>
+			<a href="#contact"><div>Contact</div></a>
 		</div>
-		<div id="about_me">
+		<div id="about_me"></div>
+		<div class="about_me">
 			<h1>Nice to meet you !</h1>
 			<p>I'm Thibault, a 19 years old developper.<br />
-			Some words to describe me : logical, cartesian, creative.<br />
+			Some words to describe me : <font size="4">logical, cartesian, creative</font>.<br />
 			So, I've always had affinity with code.<br /><br />
 			I work for me. My first motivation is to learn more and more.<br />
 			I'm student at school 42 in Paris since 6 months, <br />
@@ -43,7 +46,8 @@
 			And now it's time for me to earn professional experience.
 			</p>
 		</div>
-		<div id="career">
+		<div id="career"></div>
+		<div class="career">
 			<div class="highschool">
 				<h1>High school</h1>
 				<img src="resources/highschool.png">
@@ -67,66 +71,100 @@
 
 			</div>
 		</div>
-		<div id="skills">
+		<div id="skills"></div>
+		<div class="skills">
 			<div class="languages">
 				<h1>Languages</h1>
-				<p>French</p> ★★★★★ 
+				<div class="hov"><p>French (Mother tongue)</p> ★★★★★</div>
 				<hr>
-				<p>English</p> ★★★★
+				<div class="hov"><p>English (Written)</p> ★★★★★</div>
+				<hr>
+				<div class="hov"><p>English (Spoken)</p> ★★★★</div>
 			</div>
 			<div class="expertises">
 				<h1>Expertises</h1>
-				<p>C</p> ★★★★★
+				<div class="hov"><p>C</p> ★★★★★</div>
 				<hr>
-				<p>Shell and Shellscript</p> ★★★★★
+				<div class="hov"><p>Shell and Shellscript</p> ★★★★★</div>
 				<hr>
-				<p>Php</p> ★★★
+				<div class="hov"><p>Php</p> ★★★</div>
 				<hr>
-				<p>Css</p> ★★★
+				<div class="hov"><p>CSS(3)</p> ★★★</div>
 				<hr>
-				<p>Html</p> ★★
+				<div class="hov"><p>HTML(5)</p> ★★</div>
 			</div>
 			<div class="environments">
 				<h1>Environments</h1>
-				<p>Windows</p> ★★★★★
+				<div class="hov"><p>Windows</p> ★★★★★</div>
 				<hr>
-				<p>MacOS</p> ★★★★★
+				<div class="hov"><p>MacOS</p> ★★★★★</div>
 				<hr>
-				<p>Linux</p> ★★★
+				<div class="hov"><p>Linux</p> ★★★</div>
 				<hr>
-				<p>Ubuntu</p> ★★
+				<div class="hov"><p>Ubuntu</p> ★★</div>
 			</div>
 			<div class="software">
 				<h1>Software</h1>
-				<p>TeamViewer</p> ★★★★★
+				<div class="hov"><p>Vim <br /> Notepad++ <br /> Sublime Text</p> ★★★★★</div>
 				<hr>
-				<p>Pack Office</p> ★★★★★
+				<div class="hov"><p>Git</p> ★★★★★</div>
 				<hr>
-				<p>After Effects</p> ★★★
+				<div class="hov"><p>TeamViewer</p> ★★★★★</div>
 				<hr>
-				<p>Photoshop</p> ★★
+				<div class="hov"><p>Pack Office</p> ★★★★</div>
+				<hr>
+				<div class="hov"><p>Docker</p> ★★★</div>
+				<hr>
+				<div class="hov"><p>Adobe photoshop</p> ★★</div>
 			</div>
 		</div>
-		<div id="objectives">
-			<h1>My objectives :</h1>
+		<div id="objectives"></div>
+		<div class="objectives">
+			<div class="obj_big_title"><h1>My objectives :</h1></div>
 			<div class="for_me">
-			<h1>For me :</h1>
-			<p>The first thing I aim at is professional experience.<br />
+			<div class="obj_title"><h1>For me :</h1></div>
+			<div class="inverted"><p>The first thing I aim at is professional experience.<br />
 			I never really worked and it's time to prove my worth in company.<br />
 			Secondly, it's a way for me to learn plenty of new things.<br />
-			In internship I hope to discover new languages, softwares, and new ways to think.</p>
+			In internship I hope to discover new languages, softwares, and new ways to think.</p></div>
 			</div>
 			<div class="for_you">
-			<h1>For you :</h1>
+			<div class="obj_title"><h1>For you :</h1></div>
 			<p>As employee or trainee in your company, I will handle every mission carefully.<br />
 			My first objective is to take part of team projects.<br />
 			Our formation at school 42 used us to work as groups.<br />
 			I can lead projects well, bring ideas and respect specifications.<br /></p>
 			</div>
 		</div>
-		<div id="interests">
+		<div id="interests"></div>
+		<div class="interests">
+			<h1>My personnal interests :</h1>
+			<div class="climb">
+				<img src="resources/interest_1.jpg">
+			<hr width="200px">
+				<p><font size="4">Climbing</font> :<br /><br />Inside or outside, with friends or alone,
+				 <br />it's still the most evasive sport after a work day.</p>
+			</div>
+			<div class="computer">
+				<img src="resources/interest_2.jpg">
+			<hr width="200px">
+				<p><font size="4">Video games</font> :<br /><br /> I love video games for two thing.<br />
+				First, the competitive and technical aspects. Think better, be better.<br />
+				Secondly, there is this special community that allows you to be who you want to be.<br />
+				You can build yourself a reputation and make friends by so many ways, and I love it.
+				</p>
+			</div>
+			<div class="social">
+				<img src="resources/interest_3.png">
+			<hr width="200px">
+				<p><font size="4">Socials networks</font> :<br /><br />Nowadays, we can follow anything
+				<br />by the socials networks. It's my way to expand my knowledge.
+				<br />I use them a lot, for me it's a chance to have such powerful networks.</p>
+			</div>
 		</div>
-		<div id="contact">
+		<div id="contact"></div>
+		<div class="contact">
+			<h1>Contact me</h1>
 			<div class="pro">
 				<h1>Professional</h1>
 				<div>
@@ -142,6 +180,19 @@
 				<img src="resources/phone.png"><p>+33 7 81 37 18 88</p></a>
 				</div>
 			</div>
+			<div class="contact_form">
+			<h1>Send me an offer !</h1>
+			<p align="center"><font color="red">Still in developpement</p></font>
+			<form action="index.php" method="post" enctype="multipart/form-data">
+				<p>Company name :
+				<input type="text" name="name" id="name"></p>
+				<p>Your e-mail address :
+				<input type="text" name="mail_addr" id="mail_addr"></p>
+				<p>Offer (Upload PDF - Max 2Mo):
+				<input type="hidden" name="MAX_FILE_SIZE" value="2098576" />
+				<input type="file" name="file" id="file"><br /></p>
+			</form>	
+			</div>
 			<div class="perso">
 				<h1>Personnal</h1>
 				<div>
@@ -149,7 +200,7 @@
 				<img src="resources/mail.png"><p>lux.thibault@hotmail.fr</p></a>
 				</div>
 				<div>
-				<a href="facebook.com/luxthibault">
+				<a href="http://www.facebook.com/luxthibault">
 				<img src="resources/fb.png"><p>Facebook</p></a>
 				</div>
 				<div>
@@ -157,6 +208,8 @@
 				<img src="resources/phone.png"><p>+33 7 81 37 18 88</p></a>
 				</div>
 			</div>
+		</div>
+		<div class="footer">
 		</div>
 	</body>
 </html>
